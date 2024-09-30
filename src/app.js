190a5d7b58b12199f2f232bad6378cb97ee46211
MeetPatel-2005 <pmalay694@gmail.com>
2024-09-30 09:19:22 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const hbs = require('hbs');
 const path = require('path');
 const bcrypt = require('bcryptjs')
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
@@ -23,7 +23,7 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(partial_path);
 
 app.get('/', (req, res) => {
-    res.render('about');
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
