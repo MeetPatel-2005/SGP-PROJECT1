1f957e714e7b11047ac0dbd236d1e377f6b334b9
MeetPatel-2005 <pmalay694@gmail.com>
2024-10-02 13:23:17 +0530

    try

--- public/js/script.js
+++ public/js/script.js
@@ -13,11 +13,11 @@ const dislike = document.querySelector('#dislike');
 //   'https://source.unsplash.com/random/1000x1000/?moutain',
 //   'https://source.unsplash.com/random/1000x1000/?forest'
 // ];
+
+
 const urls = [
-  "https://m.media-amazon.com/images/I/51GOpp8rAJL._SY695_.jpg",
-  "https://images.unsplash.com/photo-1720048171596-6a7c81662434?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://github.com/MeetPatel-2005/SGP-PROJECT1/blob/main/public/images/nike.png?raw=true",
   "https://images.unsplash.com/photo-1562514947-bf9cf8e45d4a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://m.media-amazon.com/images/I/71eUwDk8z+L._SX569_.jpghttps://images.unsplash.com/photo-1709744722672-8f613bbb81e8?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1707620304878-b7d6873207f2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://plus.unsplash.com/premium_photo-1665413642308-c5c1ed052d12?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1709258228137-19a8c193be39?q=80&w=2011&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -27,11 +27,11 @@ const urls = [
 
 // variables
 let cardCount = 0;
-
+console.log(urls[cardCount % urls.length]);
 // functions
 function appendNewCard() {
   const card = new Card({
-    imageUrl: urls[cardCount % 7],
+    imageUrl: urls[cardCount % 4],
     onDismiss: appendNewCard,
     onLike: () => {
       like.style.animationPlayState = 'running';
@@ -41,7 +41,9 @@ function appendNewCard() {
       dislike.style.animationPlayState = 'running';
       dislike.classList.toggle('trigger');
     }
+    
   });
+  
   swiper.append(card.element);
   cardCount++;
 
@@ -55,4 +57,7 @@ function appendNewCard() {
 for (let i = 0; i < 5; i++) {
   appendNewCard();
 }
+
+
+
 
