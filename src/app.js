5c23aba72108e6d2076ff81497589c9103b67fe1
Jay9115 <pateljay091105@gmail.com>
2024-10-07 11:43:49 +0530

    backend connect

--- src/app.js
+++ src/app.js
@@ -1,91 +1,89 @@
 const express = require('express');
-require('./db/conn');
-const Register = require('./models/register')
-const app = express();
+const bcrypt = require('bcryptjs');
+const db = require('./firebase'); // Firebase connection
 const hbs = require('hbs');
 const path = require('path');
-const bcrypt = require('bcryptjs')
 const PORT = process.env.PORT || 6500;
+
+const app=express();
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partial_path = path.join(__dirname, "../templates/partials");
-//E:\WEBDEVELOPMENT PROJECTS\new project\SGP-PROJECT1\templates\views\partials
-console.log(partial_path);
-console.log(template_path);
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:false}));
 
 app.use(express.static(static_path));
 app.set('views', template_path);
 app.set('view engine', 'hbs');
 hbs.registerPartials(partial_path);
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+// Render pages
+app.get('/', (req, res) => res.render('home'));
+app.get('/about', (req, res) => res.render('about'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/home', (req, res) => res.render('home'));
+app.get('/Cart', (req, res) => res.render('Cart'));
+app.get('/sign', (req, res) => res.render('sign'));
 
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-app.get('/home', (req, res) => {
-    res.render('home');
-});
-
-app.get('/Cart', (req, res) => {
-    res.render('Cart');
-});
-
-app.get('/sign', (req, res) => {
-    res.render('sign');
-});
-
+// Registration (Sign Up) Handler
 app.post('/login', async (req, res) => {
-    try {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    };
 
-        const password = req.body.password;
-
-            const registerEmployee = new Register({
-                name: req.body.name,
-                email : req.body.email,
-                password : req.body.password
-            });
-
-            const registered = await registerEmployee.save();
-            res.status(200).render('home');
-
-    } catch (error) {
-        res.status(400).send(error);
-    }
+    // Save to Firebase
+    const userRef = db.collection('users').doc(req.body.email);
+    await userRef.set(newUser);
+    
+    res.status(200).render('home');
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-
+// Login Handler
+// Login Handler
 app.post('/sign', async (req, res) => {
     try {
-
-        const email = req.body.email;
-        const password = req.body.password;
-
-        const username = await Register.findOne({email:email})
-
-        if(username.password===password){
-            res.render('home');
-        }
-        else{
-            res.status(400).send(error);
-        }
-
-
+      const email = req.body.email;
+  
+      if (!email || email.trim() === "") {
+        return res.status(400).send("Email is required");
+      }
+  
+      const password = req.body.password;
+  
+      // Fetch user from Firebase
+      const userRef = db.collection('users').doc(email);
+      const doc = await userRef.get();
+  
+      if (!doc.exists) {
+        return res.status(400).send("User not found");
+      }
+  
+      const user = doc.data();
+  
+      // Compare password
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (validPassword) {
+        res.render('home');
+      } else {
+        res.status(400).send("Invalid password");
+      }
+  
     } catch (error) {
-        res.send("There is some technical issue please try agian later")
+      res.send("There is some technical issue, please try again later.");
     }
-})
+  });
+  
 
+// Listen to the port
 app.listen(PORT, () => {
-    console.log("Listening to the port ", PORT);
+  console.log("Listening to the port ", PORT);
 });
--- src/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect("mongodb+srv://23dcs082:*****@cluster0.s521t.mongodb.net/registration")
-.then(() => {
-    console.log("Connection has been setup successfully");
-})
-.catch((e) => {
-    console.log(e);
-}) 
